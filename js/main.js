ba76423f71944cdceee846b0f0d981888be4e267
sbgprojects <shubham.gujarathi@decartem.com>
2020-07-05 12:22:32 +0530

    home page complete

--- js/main.js
+++ js/main.js
@@ -89,3 +89,40 @@ $('.horizontal-slider').slick({
     vertical: true,
     verticalSwiping: true,
 });
+
+
+
+/*-------------------------------*/
+/* testimonials slider
+/*-------------------------------*/
+$('.testimonials-slider').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});

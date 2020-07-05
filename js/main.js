5d35d9248b305ede4ef1b5ebb341dba495bb7124
sbgprojects <shubham.gujarathi@decartem.com>
2020-07-05 19:03:43 +0530

    changes added

--- js/main.js
+++ js/main.js
@@ -126,3 +126,44 @@ $('.testimonials-slider').slick({
         }
     ]
 });
+
+/*-------------------------------*/
+/* youtube slider
+/*-------------------------------*/
+$('.youtube-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: $('.youtube-arrow .prev'),
+    nextArrow: $('.youtube-arrow .next'),
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
